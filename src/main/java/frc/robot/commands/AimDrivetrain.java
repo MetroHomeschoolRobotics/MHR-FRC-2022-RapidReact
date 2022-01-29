77e2ac2837e710ddb40e1d72ce5c2a1d4f811008
gmjj <gmjj.peterson@gmail.com>
2022-01-29 17:24:13 -0600

    create PID controls for aimdrivetrain; need tuning

--- src/main/java/frc/robot/commands/AimDrivetrain.java
+++ src/main/java/frc/robot/commands/AimDrivetrain.java
@@ -6,7 +6,9 @@ package frc.robot.commands;
 
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
@@ -17,6 +19,9 @@ public class AimDrivetrain extends CommandBase {
   private Vision _vision;
   private double distanceError;
   private double turnError;
+
+  private PIDController drivePID = new PIDController(0.1, 0, 0.001);
+  private PIDController aimPID = new PIDController(0.02, 0, 0.002);
   public AimDrivetrain(Vision vision, Drivetrain drivetrain) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(vision);
@@ -27,7 +32,10 @@ public class AimDrivetrain extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    SmartDashboard.putData(aimPID);
+    SmartDashboard.putData(drivePID);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -36,6 +44,7 @@ public class AimDrivetrain extends CommandBase {
       PhotonTrackedTarget target = _vision.getLimelightTarget();
       turnError = target.getYaw();
       distanceError = target.getPitch();
+      _drivetrain.moveManual(-drivePID.calculate(distanceError,0),-aimPID.calculate(turnError, 0));
     }
   }
 
