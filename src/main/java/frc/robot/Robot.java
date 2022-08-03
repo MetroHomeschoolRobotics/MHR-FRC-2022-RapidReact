97b466ec6645bfb1cd5b496e498246037738d01a
George Horsey <ghorsey@metrohomeschoolrobotics.com>
2022-08-02 19:48:30 -0500

    Add CommandScheduler to SmartDashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,11 @@ package frc.robot;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -31,6 +34,9 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
     //CommandScheduler.getInstance().schedule(new SetBrakeMode(RobotContainer.s_drivetrain, false));
+
+    // added to see current commands
+    SmartDashboard.putData(CommandScheduler.getInstance());
   }
 
   /**
@@ -86,6 +92,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
+    
   }
 
   /** This function is called periodically during operator control. */
